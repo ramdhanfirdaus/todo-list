822f65c96cb5b8300602dbf60500989841e14918
Ramdhan Firdaus Amelia <ramdhan.firdaus@ui.ac.id>
2023-08-12 13:28:32 +0700

    Set up vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+
+async function generatePrismaClient() {
+  const prisma = new PrismaClient();
+  try {
+    await prisma.$connect();
+    await prisma.$disconnect();
+  } catch (error) {
+    console.error('Error generating Prisma Client:', error);
+    throw error;
+  }
+}
 
 async function bootstrap() {
+
+  if (process.env.PRISMA_GENERATE_ON_BUILD === 'true') {
+    await generatePrismaClient();
+    console.log('Prisma Client generation successful.');
+  }
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
